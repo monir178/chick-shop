02b2721c52660362e700158775a2a022494e4fba
monir178 <monir.mzs17@gmail.com>
2024-09-21 01:46:54 +0600

    updated sign in , sign-up page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center my-20 md:my-0 justify-center">
       <SignUp />
     </div>
   );
--- components/Products.tsx
+++ components/Products.tsx
@@ -17,7 +17,7 @@ const Products = async () => {
       {!products || products.length === 0 ? (
         <p className="text-gray-800 mt-4">No Collections found</p>
       ) : (
-        <div className="grid grid-cols-2 gap-8 md:gap-10 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 ">
+        <div className="grid grid-cols-2 gap-4  md:gap-10 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 ">
           {products.map((product: TProductType) => (
             <ProductCard key={product._id} product={product} userId={userId} />
           ))}
